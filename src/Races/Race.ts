857bf6310b0561a3e80cb5e7be1480bf102ae978
Jean Viegas <jeanmelg@gmail.com>
2022-06-27 15:28:15 -0300

    feat: abstract class for Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,19 @@
+export default abstract class Race {
+  private static racesCount = 0;
+  constructor(
+    private _name: string,
+    private _dexterity: number,
+  ) {
+    Object.assign(this, { _name, _dexterity });
+  }
+
+  get name(): string { return this._name; }
+
+  get dexterity(): number { return this._dexterity; }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
